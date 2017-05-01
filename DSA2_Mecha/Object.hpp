3b2fe50ae19e3eb24f27d02f13239f60b34be3e1
Ryan Bell <rjb1928@rit.edu>
2017-05-01 03:00:14 -0400

    added base object for inheritance (modeled after unity)

---
+++ DSA2_Mecha/Object.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "RE\ReEngAppClass.h"
+
+class Object {
+private:
+	static std::vector<int> instanceIDs;
+	std::string name;
+	int instanceID;
+public:
+	Object();
+	Object(const Object& other);
+	~Object();
+	int GetInstanceID() const;
+	std::string ToString();
+	Object Clone();
+	static void Destroy(Object obj, float delay = 0.0f);
+	static void DontDestroyOnLoad(Object target);
+	static Object Instantiate(Object original);
+	static Object Instantiate(Object original, vector3 position, quaternion rotation);
+	bool operator!=(Object& other);
+	bool operator==(Object& other);
+	operator bool() const;
+};
